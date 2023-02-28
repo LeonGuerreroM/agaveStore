266992e2f3f12045b77c9446a7e4e7b10e45c197
León Mora <leonmora19@outlook.com>
2023-02-28 17:32:42 -0600

    project setup

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const config = require('./config');
+
+app.use(express.json());
+
+
+app.listen(config.port, ()  => {
+    console.log('listening on port '+config.port);
+})
