0e3014e29e0ab56c4417a28a3384fe38ef3eba27
LeonGuerreroM <leonmora19@outlook.com>
2023-03-02 13:41:49 -0600

    changes to deploy

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const config = {
+    isProd: process.env.NODE_ENV === 'production',
     port: process.env.PORT || 3000,
     dbUrl: process.env.DATABASE_URL,
     suPassword: process.env.SAMPLE_USER_PASSWORD,
